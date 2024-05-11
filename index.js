85c63274576f7ff90f9d147ddc4a688fc80067da
Yacine <yacinebenalidayat@gmail.com>
2024-05-11 14:44:59 +0100

    update 1
      - optimize structure
      - add bootstrap 5

--- index.js
+++ index.js
@@ -17,36 +17,12 @@ app.get('/', (_req, res) => {
 app.post('/items', (req, res) => {
   let startNode = parseInt(req.body.startNode)
   let endNode = parseInt(req.body.endNode)
-  console.log({ startNode, endNode })
 
   let html = ""
   for (let index = startNode + 1; index <= endNode; index++) {
-    html += `<li id="item-${index}">Item: ${index}</li>`
+    html += `<li class="list-group-item" data-user-id="${index}">Item: ${index}</li>`
   }
   res.send(html)
-
-  // res.send(`<li id="item-${index}">Item: ${index}</li>`)
-  // res.send(`
-  //   <li id="item-1">Item: 1</li>
-  //   <li id="item-2">Item: 2</li>
-  //   <li id="item-3">Item: 3</li>
-  //   <li id="item-4">Item: 4</li>
-  //   <li id="item-5">Item: 5</li>
-  //   <li id="item-6">Item: 6</li>
-  //   <li id="item-7">Item: 7</li>
-  //   <li id="item-8">Item: 8</li>
-  //   <li id="item-9">Item: 9</li>
-  //   <li id="item-10">Item: 10</li>
-  //   <li id="item-11">Item: 11</li>
-  //   <li id="item-12">Item: 12</li>
-  //   <li id="item-13">Item: 13</li>
-  //   <li id="item-14">Item: 14</li>
-  //   <li id="item-15">Item: 15</li>
-  //   <li id="item-16">Item: 16</li>
-  //   <li id="item-17">Item: 17</li>
-  //   <li id="item-18">Item: 18</li>
-  //   <li id="item-19">Item: 19</li>
-  //   <li id="item-20">Item: 20</li>`)
 })
 
 app.post('/item/:index', (req, res) => {
--- public/app.js
+++ public/app.js
@@ -1,44 +1,63 @@
-const list = document.querySelector("#list")
+const nodesList = document.querySelector(".nodes-list")
+const nodesListWrapper = document.querySelector(".nodes-list-wrapper")
+const visibleNodesListWrapper = document.querySelector(".visible-nodes-list-wrapper")
+const ul = document.querySelector("ul")
 
 document.addEventListener("DOMContentLoaded", _ => {
-  htmx.trigger("#list", "scroll")
+  htmx.trigger(".nodes-list", "scroll")
 })
 
-const container = document.querySelector(".container")
-const visibleNodesContainer = document.querySelector(".visible-nodes-container")
+// todo large numbers
+let itemCount = 100_000
+let rowHeight = 41
 
-let itemCount = 1_000_000
-let rowHeight = 20
+const viewportHeight = nodesList.clientHeight
 
-const totalContentHeight = itemCount * rowHeight;
-container.style.height = `${totalContentHeight}px`
+let scrollTop = nodesList.scrollTop
+let visibleNodesCount = getVisibleNodesCount({rowHeight, viewportHeight})
+let startNode = getStartNode(scrollTop)
+let endNode = getEndNode(startNode, visibleNodesCount, itemCount)
 
-let startNode = Math.floor(list.scrollTop / rowHeight);
-startNode = Math.max(0, startNode)
+setNodesListWrapperHeight({ itemCount, rowHeight })
+setOffsetYToVisibleNodesContainer({ startNode, rowHeight })
 
-let offsetY = startNode * rowHeight;
+nodesList.addEventListener("htmx:configRequest", evt => {
+  scrollTop = nodesList.scrollTop
+  startNode = getStartNode(scrollTop)
+  endNode = getEndNode(startNode, visibleNodesCount, itemCount)
+  setParameters(evt, { startNode, endNode })
+})
 
-let visibleNodesCount = Math.ceil(list.clientHeight / rowHeight);
-visibleNodesCount = Math.min(itemCount - startNode, visibleNodesCount);
-// visibleNodesCount = Math.min(itemCount, visibleNodesCount);
+nodesList.addEventListener("htmx:beforeSwap", _ => {
+  setOffsetYToVisibleNodesContainer({ startNode, rowHeight })
+})
 
-console.log({ itemCount, totalContentHeight, visibleNodesCount })
+function setNodesListWrapperHeight ({ itemCount, rowHeight }) {
+  nodesListWrapper.style.height = `${itemCount * rowHeight}px`
+}
 
-let endNode = startNode + visibleNodesCount;
+function getVisibleNodesCount ({ rowHeight, viewportHeight}) {
+  return Math.ceil(viewportHeight / rowHeight)
+}
 
+function setOffsetYToVisibleNodesContainer ({ startNode, rowHeight }) {
+  let offsetY = startNode * rowHeight;
+  visibleNodesListWrapper.style.transform = `translateY(${offsetY}px)`
+}
 
-list.addEventListener("htmx:configRequest", evt => {
-  startNode = Math.floor(list.scrollTop / rowHeight)
-  startNode = Math.max(0, startNode);
+function getStartNode (scrollTop) {
+  let startNode = Math.floor(scrollTop / rowHeight) - 1
+  startNode = Math.max(0, startNode)
+  return startNode
+}
 
-  endNode = startNode + visibleNodesCount;
+function getEndNode (startNode, visibleNodesCount, itemCount) {
+  let endNode = startNode + visibleNodesCount + 2;
   endNode = Math.min(itemCount, endNode)
+  return endNode
+}
 
-  offsetY = startNode * rowHeight;
-
-  visibleNodesContainer.style.transform = `translateY(${offsetY}px)`
-
-  evt.detail.parameters['startNode'] = startNode
-  evt.detail.parameters['endNode'] = endNode
-})
-
+function setParameters (evt, { startNode, endNode }) {
+  evt.detail.parameters.startNode = startNode
+  evt.detail.parameters.endNode = endNode
+}
